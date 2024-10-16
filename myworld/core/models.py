9c6b683b9059bb06200b7097c7c399a23aa8a806
fb0801 <fbhatti0801@gmail.com>
2024-10-16 17:59:08 +0100

    creating the db

--- myworld/core/models.py
+++ myworld/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField('profile', null=True, blank=True)
+
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
