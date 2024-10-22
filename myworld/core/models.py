ddfe420c6a9e64a1b6401b5a6be2597436518af7
fb0801 <fbhatti0801@gmail.com>
2024-10-22 17:20:22 +0100

    creating the db 2

--- myworld/core/models.py
+++ myworld/core/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+
+AGE_CHOICES = (
+    ('All','All'),
+    ('Kids','Kids')
+)
+
 
 class CustomUser(AbstractUser):
     profiles = models.ManyToManyField('profile', null=True, blank=True)
 
 
-
 class Profile(models.Model):
     name = models.CharField(max_length=255)
+    age_limit = models.CharField(max_length=10, choices=AGE_CHOICES)
+    uuid=models.UUIDField(default=uuid.uuid4)
