a17c15b154077c90ace3f9d10bc5941c116cbd87
fb0801 <fbhatti0801@gmail.com>
2024-10-23 17:42:49 +0100

    creating the db 2.2

--- myworld/core/models.py
+++ myworld/core/models.py
@@ -8,6 +8,11 @@ AGE_CHOICES = (
     ('Kids','Kids')
 )
 
+MOVIE_CHOICES =(
+    ('seasonal','Seasonal'),
+    ('single','Single')
+)
+
 
 class CustomUser(AbstractUser):
     profiles = models.ManyToManyField('profile', null=True, blank=True)
@@ -21,4 +26,10 @@ class Profile(models.Model):
 
 class Movie(models.Model):
     title=models.CharField(max_length=225)
-    description=
+    description=models.TextField(blank=True, null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    uuid=models.UUIDField(default=uuid.uuid4)
+    type= models.CharField(max_length=10,choices=MOVIE_CHOICES)
+    videos=models.ManyToManyField('Video')
+    flyer=models.ImageField(upload_to='flyers')
+    age_limit=models.CharField(max_length=10,choices=AGE_CHOICES)
