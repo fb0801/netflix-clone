c2f90bab699c030f9d598017d823e380eec4cce1
fb0801 <fbhatti0801@gmail.com>
2024-10-30 17:47:11 +0000

    auth

--- myworld/django_netflix/settings.py
+++ myworld/django_netflix/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
 #auth setting
 AUTH_USER_MODEL = 'core.CustomUser'
 
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
